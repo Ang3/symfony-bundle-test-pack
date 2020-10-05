373a43dc9262992b4909ed80828fb71cc1d1fd2d
Joanis Rouanet <joanis.ang3@gmail.com>
2020-10-05 19:06:23 +0200

    Testing

---
+++ src/Composer/ScriptHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ang3\Bundle\Test\Composer;
+
+use Composer\Script\Event;
+
+class ScriptHandler
+{
+    /**
+     * @param Event $event
+     */
+    public static function copy(Event $event)
+    {
+        $composer = $event->getComposer();
+        $extras = $composer->getPackage()->getExtra();
+        $files = $extras['copy-file'];
+        $io = $event->getIO();
+
+        if (!$files) {
+            $io->write("No dirs or files are configured through the composer extra section.");
+
+            return;
+        }
+
+        $composerConfig = $composer->getConfig();
+        $vendorDir = $composerConfig->get('vendor-dir');
+
+        var_dump($vendorDir); die;
+    }
+}
